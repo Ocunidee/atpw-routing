9c17bcd1b56ebf46ba3038c16303efd2de3e4c4e
Ocunidee <ocunidee@gmail.com>
2025-02-13 16:10:36 +0100

    Fix memory leak

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core'
+import { Component, DestroyRef, OnDestroy, OnInit } from '@angular/core'
 import { bookDetails } from '../data/book-details'
 import { BookDetail } from '../models/book-detail'
 import { ActivatedRoute, RouterLink } from '@angular/router'
 import { interval } from 'rxjs'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-book-details',
@@ -13,13 +14,17 @@ import { interval } from 'rxjs'
 export class BookDetailsComponent implements OnInit, OnDestroy {
   details: BookDetail | undefined
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private destroyRef: DestroyRef
+  ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id')
     this.details = bookDetails.get(Number(id))
 
     interval(1000)
+      .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({ next: counter => console.log(counter) })
   }
 
