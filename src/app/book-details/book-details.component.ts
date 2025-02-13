a8fc4bc575f926dbb5f504bba91275f4a3796ecb
Ocunidee <ocunidee@gmail.com>
2025-02-13 16:02:13 +0100

    Introduce memory leak

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit, signal } from '@angular/core'
+import { Component, inject, OnInit, signal, OnDestroy } from '@angular/core'
 import { bookDetails } from '../data/book-details'
 import { BookDetail } from '../models/book-detail'
 import { ActivatedRoute, RouterLink } from '@angular/router'
+import { interval } from 'rxjs'
 
 @Component({
   selector: 'app-book-details',
@@ -9,12 +10,19 @@ import { ActivatedRoute, RouterLink } from '@angular/router'
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent implements OnInit, OnDestroy {
   private readonly route = inject(ActivatedRoute)
   protected readonly details = signal<BookDetail | undefined>(undefined)
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id')
     this.details.set(bookDetails.get(Number(id)))
+
+    interval(1000)
+      .subscribe({ next: counter => console.log(counter) })
+  }
+
+  ngOnDestroy(): void {
+    console.log('Component destroyed')
   }
 }
